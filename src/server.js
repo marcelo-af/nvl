9a8a1512203b8c55d9a34d31e4863230292da4e9
Marcelo Alves Ferreira <marceloalvesferreira265@gmail.com>
2020-06-08 00:32:25 -0300

    metade video aula 5

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+//pegar o banco de dados que foi exportado do db.js
+const db = require("./database/db.js")
+
 // donfigurar pasta public onde estao as pastas (assets, scripts, styles)
 server.use(express.static("public")) 
 
@@ -27,14 +30,37 @@ server.get("/", (req, res) => {
 
 //chamada da rota da pagina create-point
 server.get("/create-point", (req, res) => {
+    console.log(req.query)
     return res.render("create-point.html")  // renderiza o caminho da página pq o nunjucks foi instalado
     //res.sendFile(__dirname + "/views/create-point.html" ) //envia o caminho da pagina
 })
 
+//chamada do post no create-point
+server.post("/savepoint", (req, res) => {
+    return res.send("ok")
+})
+
 //chamada da rota da pagina search-results
 server.get("/search-results", (req, res) => {
-    return res.render("search-results.html") // renderiza o caminho da página pq o nunjucks foi instalado
+//pegar os dados do banco de dados 
+db.all(`SELECT * FROM places`, function(err, rows){
+    if(err) {
+        return console.log(err)
+    }
+    
+    console.log("Aqui estão seus registros:")
+    console.log(rows)
+
+    const total = rows.length
+
+    // mostrar a página html com os dados do banco de dados
+    return res.render("search-results.html", {places : rows, total: total} ) 
    // res.sendFile(__dirname + "/views/search-results.html" ) //envia o caminho da pagina
+    
+    })
+
+
+    
 })
 
 
